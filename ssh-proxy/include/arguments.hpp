58cb95e2ee8e887fc2ec1f910d8736887d2e08c2
Pixel <scott.blatt.0b10@gmail.com>
2025-04-05 14:51:29 -0500

    changes to arg processor

--- ssh-proxy/include/arguments.hpp
+++ ssh-proxy/include/arguments.hpp
@@ -11,14 +11,25 @@ namespace arguments {
         int argc;
         char** argv;
       };
-      cArgStruct argc;
+      struct argumentIdent {
+        string head;
+        string delimiter;
+      };
+    private:
+      const cArgStruct argc;
+      const char** envc;
       std::vector<string> argv;
       std::map<string,string> argm;
-      virtual void argumentProcessor(string head, char splitter) { // Allow overriding if someone else wants to make a different processor
+      void argumentVectorizer() {
+        for (int i = 0; i < this->argc.argc; i++) {
+          this->argv.push_back(this->argc.argv[i]);
+        }
+      }
+      virtual void argumentMapper(argumentIdent ident) { // Allow overriding if someone else wants to make a different processor
         for (const string& argument : argv) {
-          if (argument.starts_with(head) && argument.contains(splitter)) {
-            string name = argument.substr(head.length(), argument.find(splitter) - head.length());
-            string content = argument.substr(argument.find(splitter) + 1);
+          if (argument.starts_with(ident.head) && argument.contains(ident.delimiter)) {
+            string name = argument.substr(ident.head.length(), argument.find(ident.delimiter) - ident.head.length());
+            string content = argument.substr(argument.find(ident.delimiter) + ident.delimiter.length());
             if (!name.empty() && !content.empty()) {
               argm.insert({name, content});
             }
@@ -26,12 +37,9 @@ namespace arguments {
         }
       }
     public:
-      arguments(int argc, char** argv): argc({argc, argv}) { // Store cArgs for later and legacy
-        // Vectorise arguments
-        for (int i = 0; i < this->argc.argc; i++) {
-          this->argv.push_back(this->argc.argv[i]);
-        }
-        argumentProcessor("--", '=');
+      arguments(int argc, char** argv, char** envp = {}, argumentIdent delimiters = {"--", "="}): argc({argc, argv}) {
+        argumentVectorizer();
+        argumentMapper(delimiters);
       };
       cArgStruct cArgs() { return argc; }
       map<string,string> map() { return argm; }
