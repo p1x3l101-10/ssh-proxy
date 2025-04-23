bdf989d5b57f39a3b9087ea44c078d231bd85d3e
Pixel <scott.blatt.0b10@gmail.com>
2025-04-22 20:12:31 -0500

    move debug statements

--- ssh-proxy/src/socks5Session/connectLocal.cpp
+++ ssh-proxy/src/socks5Session/connectLocal.cpp
@@ -9,9 +9,13 @@ using boost::asio::ip::tcp;
 void sshProxy::socks5Session::connectLocal(socks5Values::clientConnect &connection) {
   auto self = shared_from_this();
   createLogger(logger);
+  logger.debugStream() << "Connection diagram - IP: " << connection.destinationAddress.string() << ", Port: " << connection.destinationPort.string() << ", Address Type: " << magic_enum::enum_name(connection.destinationAddress.type);
   if (connection.destinationAddress.type != socks5Values::addressType::DOMAIN_NAME) {
     logger.debug("Skipping resolution");
-    tcp::endpoint endpoint = { boost::asio::ip::make_address(connection.destinationAddress.string()), connection.destinationPort.portNum };
+    tcp::endpoint endpoint = {
+      boost::asio::ip::make_address(connection.destinationAddress.string()),
+      connection.destinationPort.portNum
+    };
     // Start the connection timer
     connectTimer.expires_after(std::chrono::seconds(CONNECTION_TIMEOUT_SECONDS));
     connectTimer.async_wait([this, self](boost::system::error_code ec){
@@ -29,7 +33,6 @@ void sshProxy::socks5Session::connectLocal(socks5Values::clientConnect &connecti
       createLogger(logger);
       if (!ec) {
         logger.debug("Created remote socket");
-        logger.debugStream() << "Connection diagram - IP: " << connection.destinationAddress.string() << ", Port: " << connection.destinationPort.string() << ", Address Type: " << magic_enum::enum_name(connection.destinationAddress.type);
         // Acknowlage success
         socks5Values::connectResponce ack = socks5Values::responceStatus::GRANTED;
         // Start connection
@@ -38,40 +41,40 @@ void sshProxy::socks5Session::connectLocal(socks5Values::clientConnect &connecti
         startRemoteToClientRelay();
       } else { errorhander(ec, logger.getName()); }
     });
-    return;
+  } else {
+    logger.debug("Resolving endpoints");
+    resolver.async_resolve(connection.destinationAddress.string(), connection.destinationPort.string(),
+      [this, self, connection](boost::system::error_code ec, tcp::resolver::results_type results){
+        createLogger(logger);
+        if (!ec) {
+          logger.debug("Resolved endpoints");
+          connectTimer.expires_after(std::chrono::seconds(CONNECTION_TIMEOUT_SECONDS));
+          connectTimer.async_wait([this, self](boost::system::error_code ec){
+            createLogger(logger);
+            if (!ec) {
+              logger.error("Timed out");
+              remoteSocket.cancel(); // Timed out
+              socks5Values::connectResponce failure = socks5Values::responceStatus::TTL_EXPIRED;
+              auto ret = async_write(clientSocket, boost::asio::buffer(failure.data()));
+              closeBoth();
+              } else { errorhander(ec, logger.getName()); }
+          });
+          boost::asio::async_connect(remoteSocket, results, [this, self, connection](boost::system::error_code ec, const tcp::endpoint&){
+            connectTimer.cancel(); // Success
+            createLogger(logger);
+            if (!ec) {
+              logger.debug("Created remote socket");
+              logger.debugStream() << "Connection diagram - IP: " << connection.destinationAddress.string() << ", Port: " << connection.destinationPort.string() << ", Address Type: " << magic_enum::enum_name(connection.destinationAddress.type);
+              // Acknowlage success
+              socks5Values::connectResponce ack = socks5Values::responceStatus::GRANTED;
+              // Start connection
+              logger.debug("Started data transfer");
+              startClientToRemoteRelay();
+              startRemoteToClientRelay();
+            } else { errorhander(ec, logger.getName()); }
+          });
+        } else { errorhander(ec, logger.getName()); }
+      }
+    );
   }
-  logger.debug("Resolving endpoints");
-  resolver.async_resolve(connection.destinationAddress.string(), connection.destinationPort.string(),
-    [this, self, connection](boost::system::error_code ec, tcp::resolver::results_type results){
-      createLogger(logger);
-      if (!ec) {
-        logger.debug("Resolved endpoints");
-        connectTimer.expires_after(std::chrono::seconds(CONNECTION_TIMEOUT_SECONDS));
-        connectTimer.async_wait([this, self](boost::system::error_code ec){
-          createLogger(logger);
-          if (!ec) {
-            logger.error("Timed out");
-            remoteSocket.cancel(); // Timed out
-            socks5Values::connectResponce failure = socks5Values::responceStatus::TTL_EXPIRED;
-            auto ret = async_write(clientSocket, boost::asio::buffer(failure.data()));
-            closeBoth();
-          } else { errorhander(ec, logger.getName()); }
-        });
-        boost::asio::async_connect(remoteSocket, results, [this, self, connection](boost::system::error_code ec, const tcp::endpoint&){
-          connectTimer.cancel(); // Success
-          createLogger(logger);
-          if (!ec) {
-            logger.debug("Created remote socket");
-            logger.debugStream() << "Connection diagram - IP: " << connection.destinationAddress.string() << ", Port: " << connection.destinationPort.string() << ", Address Type: " << magic_enum::enum_name(connection.destinationAddress.type);
-            // Acknowlage success
-            socks5Values::connectResponce ack = socks5Values::responceStatus::GRANTED;
-            // Start connection
-            logger.debug("Started data transfer");
-            startClientToRemoteRelay();
-            startRemoteToClientRelay();
-          } else { errorhander(ec, logger.getName()); }
-        });
-      } else { errorhander(ec, logger.getName()); }
-    }
-  );
 }
