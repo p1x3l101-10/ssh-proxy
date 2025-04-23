e9062be313ff39395679e3cbd59da2146ab93df6
Pixel <scott.blatt.0b10@gmail.com>
2025-04-22 20:12:00 -0500

    fix bad string

--- ssh-proxy/src/socks5Values/address/string.cpp
+++ ssh-proxy/src/socks5Values/address/string.cpp
@@ -5,21 +5,21 @@ const std::string socks5Values::address::string() const {
   std::stringstream out;
   switch (type) {
     case socks5Values::addressType::IPV4: {
-      for (int i = 0; i < addr.size(); i--) {
+      for (int i = 0; i < addr.size(); i++) {
         out << static_cast<int>(addr.at(i));
-        if (i != 0) {
+        if (i < addr.size() - 1) {
           out << ".";
         }
       }
       break;
     }
     case socks5Values::addressType::IPV6: {
-      for (int i = 0; i < addr.size(); i = i - 2) {
+      for (int i = 0; i < addr.size(); i = i + 2) {
         out << std::hex
             << static_cast<int>(addr.at(i + 1))
             << static_cast<int>(addr.at(i))
             << std::dec;
-        if (i != 0) {
+        if (i < addr.size() - 2) {
           out << "::";
         }
       }
