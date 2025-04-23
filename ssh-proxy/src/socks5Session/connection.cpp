46ed58d15e015d968d6c05394a114433af1e3282
Pixel <scott.blatt.0b10@gmail.com>
2025-04-22 20:12:08 -0500

    fix fails

--- ssh-proxy/src/socks5Session/connection.cpp
+++ ssh-proxy/src/socks5Session/connection.cpp
@@ -9,6 +9,9 @@ void sshProxy::socks5Session::connection(socks5Values::clientConnect &connection
   if (ssh_is_connected(session->getCSession()) == 0) {
     logger.warn("Ssh server is offline, not attempting to proxy traffic. Expect blocks");
     connectLocal(connection);
+    // Some failure
+    closeBoth();
+    return;
   }
   // Test if site is blocked
   if (isBlocked(connection.destinationAddress.string())) {
