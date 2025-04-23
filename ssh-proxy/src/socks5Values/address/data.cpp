7a856177cdf89b93db3cd051973e8b23de242a2e
Pixel <scott.blatt.0b10@gmail.com>
2025-04-22 21:03:38 -0500

    fix vars

--- ssh-proxy/src/socks5Values/address/data.cpp
+++ ssh-proxy/src/socks5Values/address/data.cpp
@@ -1,7 +1,8 @@
 #include "socks5Values/address.hpp"
+#include <iostream>
 
 const std::vector<uint8_t> socks5Values::address::data() const {
-  std::vector<uint8_t> out(1 + addr.size()); // Reserve size needed for vector
+  std::vector<uint8_t> out; // Reserve size needed for vector
   out.reserve(1 + addr.size());
   out.push_back(static_cast<uint8_t>(type));
   out.insert(out.end(), addr.begin(), addr.end());
