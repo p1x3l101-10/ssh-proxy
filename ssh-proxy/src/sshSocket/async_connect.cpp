f2309b7aaaaba53250c9b3ffd2bdec55b1c06a0c
Pixel <scott.blatt.0b10@gmail.com>
2025-04-23 15:45:03 -0500

    make sure the connection is open

--- ssh-proxy/src/sshSocket/async_connect.cpp
+++ ssh-proxy/src/sshSocket/async_connect.cpp
@@ -1,21 +1,43 @@
 #include "sshProxy/sshSocket.hpp"
+#include <cerrno>
 #include <mutex>
 #include <thread>
-#include <iostream>
 
 void sshProxy::sshSocket::async_connect(const boost::asio::ip::tcp::endpoint& endpoint, std::function<void(boost::system::error_code)> handler) {
   std::thread([&mtx = this->mtx, &isConnected = this->isConnected, channel = this->channel, executor = this->executor, handler = std::move(handler), endpoint]() mutable {
     std::scoped_lock lock(mtx);
     try {
       // Open a connection
-      auto ip = endpoint.address().to_string();
+      auto address = endpoint.address().to_string();
       auto port = endpoint.port();
-      std::cout << ip << ", " << port << std::endl;
-      channel->openForward(ip.c_str(), port, "127.0.0.1", 0);
-      isConnected = true;
-      boost::asio::post(executor, [handler = std::move(handler)]() {
-        handler(boost::system::error_code{}); // success
-      });
+      channel->openForward(address.c_str(), port, "127.0.0.1", 0);
+      // Test for the connection to be on
+      try {
+        while (true) { // Start a loop to wait for the server to be ready
+          int ret = channel->poll();
+          if (ret > 0) {
+            isConnected = true;
+            break;
+          } else if (ret < 0) {
+            break;
+          }
+        }
+      } catch (ssh::SshException &ex) {
+        if (ex.getCode() == EAGAIN) {
+          isConnected = true;
+        } else {
+          throw ex;
+        }
+      }
+      if (isConnected) {
+        boost::asio::post(executor, [handler = std::move(handler)]() {
+          handler(boost::system::error_code{}); // success
+        });
+      } else {
+        boost::asio::post(executor, [handler = std::move(handler)]() {
+          handler(boost::system::error_code{boost::system::errc::io_error, boost::system::generic_category()});
+        });
+      }
     } catch (ssh::SshException& ex) {
       boost::asio::post(executor, [&ex, handler = std::move(handler)]() {
         handler(boost::system::error_code{ex.getCode(), boost::system::generic_category()});
@@ -30,10 +52,33 @@ void sshProxy::sshSocket::async_connect(const std::string address, const uint16_
     try {
       // Open a connection
       channel->openForward(address.c_str(), port, "127.0.0.1", 0);
-      isConnected = true;
-      boost::asio::post(executor, [handler = std::move(handler)]() {
-        handler(boost::system::error_code{}); // success
-      });
+      // Test for the connection to be on
+      try {
+        while (true) { // Start a loop to wait for the server to be ready
+          int ret = channel->poll();
+          if (ret > 0) {
+            isConnected = true;
+            break;
+          } else if (ret < 0) {
+            break;
+          }
+        }
+      } catch (ssh::SshException &ex) {
+        if (ex.getCode() == EAGAIN) {
+          isConnected = true;
+        } else {
+          throw ex;
+        }
+      }
+      if (isConnected) {
+        boost::asio::post(executor, [handler = std::move(handler)]() {
+          handler(boost::system::error_code{}); // success
+        });
+      } else {
+        boost::asio::post(executor, [handler = std::move(handler)]() {
+          handler(boost::system::error_code{boost::system::errc::io_error, boost::system::generic_category()});
+        });
+      }
     } catch (ssh::SshException& ex) {
       boost::asio::post(executor, [&ex, handler = std::move(handler)]() {
         handler(boost::system::error_code{ex.getCode(), boost::system::generic_category()});
