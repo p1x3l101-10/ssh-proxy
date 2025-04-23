b6f337f2441da3c51834dc4cbdd35075c139ad1b
Pixel <scott.blatt.0b10@gmail.com>
2025-04-22 21:04:20 -0500

    fixes

--- ssh-proxy/src/socks5Session/startClientToRemoteRelay.cpp
+++ ssh-proxy/src/socks5Session/startClientToRemoteRelay.cpp
@@ -4,11 +4,11 @@
 void sshProxy::socks5Session::startClientToRemoteRelay() {
   createLogger(logger);
   auto self(shared_from_this());
-  auto buffer = std::make_shared<std::vector<uint8_t>>();
+  auto buffer = std::make_shared<std::vector<uint8_t>>(4096);
   clientSocket.async_read_some(boost::asio::buffer(*buffer),
     [this, self, buffer](boost::system::error_code ec, std::size_t length){
       if (!ec) {
-        async_write(remoteSocket, boost::asio::buffer(*buffer),
+        async_write(remoteSocket, boost::asio::buffer(buffer->data(), length),
           [this, self](boost::system::error_code ec, std::size_t length){
             if (!ec) {
               startClientToRemoteRelay();
