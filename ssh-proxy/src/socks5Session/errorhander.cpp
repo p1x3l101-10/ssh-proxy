b10c3ef44d668152539cd27b0c29f769fe99dc6a
Pixel <scott.blatt.0b10@gmail.com>
2025-04-22 21:06:12 -0500

    better logic for eof

--- ssh-proxy/src/socks5Session/errorhander.cpp
+++ ssh-proxy/src/socks5Session/errorhander.cpp
@@ -1,6 +1,10 @@
 #include "sshProxy/socks5Session.hpp"
 #include "socks5Values/connectResponce.hpp"
 #include <log4cpp/Category.hh>
+#include <unistd.h>
+
+namespace err = boost::asio::error;
+using socks5Values::responceStatus;
 
 void sshProxy::socks5Session::errorhander(boost::system::error_code &ec, const std::string loggerName) {
   // Create logger from other file, and signify this is in the errorhandler function
@@ -8,17 +12,18 @@ void sshProxy::socks5Session::errorhander(boost::system::error_code &ec, const s
   // Figure out error from boost error code
   socks5Values::responceStatus code;
   switch (ec.value()) {
-    namespace err = boost::asio::error;
-    using socks5Values::responceStatus;
     case err::operation_aborted: return; // Stop telling me that the stupid timers were stopped
     case err::host_not_found:
     case err::host_unreachable: code = responceStatus::HOST_UNREACHABLE; break;
     case err::network_unreachable: code = responceStatus::NETWORK_UNREACHABLE; break;
-    case err::eof:
     case err::connection_refused: code = responceStatus::CONNECTION_REFUSED_BY_DEST; break;
     case err::shut_down:
     case err::timed_out: code = responceStatus::TTL_EXPIRED; break;
     default: code = responceStatus::GENERAL_FAILURE; break;
+  }
+  if (ec.value() == err::eof) {
+    logger.debug("Connection terminated.");
+    closeBoth();
   }
   logger.errorStream() << ec.message();
   socks5Values::connectResponce failure = code;
