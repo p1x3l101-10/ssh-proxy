aa8b886ee60b88ddce6d1262324de8ac75c31dea
Pixel <scott.blatt.0b10@gmail.com>
2025-04-23 17:47:59 -0500

    docs

--- ssh-proxy/src/tryConnect.cpp
+++ ssh-proxy/src/tryConnect.cpp
@@ -5,6 +5,7 @@
 using boost::asio::ip::tcp;
 
 void try_connect(std::shared_ptr<asyncStream> socket, tcp::resolver::results_type results, std::function<void(boost::system::error_code,const tcp::endpoint)> handler) {
+  // A stand-in for the boost asio resolver, it does not like my polymorphic class
   auto iter = std::make_shared<tcp::resolver::results_type::iterator>(results.begin());
   auto end = results.end();
 
